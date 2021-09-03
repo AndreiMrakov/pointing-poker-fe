f27e779b3558121036be166bdc4adfbca93be007
Yury Bialou <Pradd.by@gmail.com>
2021-09-03 17:41:42 +0300

    feat: add game and components

---
+++ src/components/game/panel-control.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const GameControlPanel = () => {
+  return <div>Panel Control</div>;
+};
+
+export default GameControlPanel;
+
+
+
+
+
+
+
+
+
+
+
+
+// game control panel: restart,finish,start game buttons
