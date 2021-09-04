058cb14dfc2ca75c6c57077b20cea40fce9cd566
Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
2021-09-04 22:00:43 +0300

    feat: add response's interceptor and base requests

---
+++ src/interfaces.ts
@@ -0,0 +1,14 @@
+export interface ITask {
+  value: string;
+  id?: number;
+}
+
+export interface IUser {
+  name: string;
+  id?: number;
+}
+
+export interface ICard {
+  value: number;
+  id?: number;
+}
