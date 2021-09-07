7f44b9eb17b838c0ed9a4817641a84f8525786b8
Yury Bialou <Pradd.by@gmail.com>
2021-09-07 22:34:57 +0300

    refactor: refactor getAvatar

--- src/helpers/getAvatar.ts
+++ src/helpers/getAvatar.ts
@@ -1,9 +1,5 @@
 import defaultAvatar from '@/assets/images/default-avatar.svg';
 
-export const getAvatar = (avatar: string) => {
-  const avatarUser = avatar ? avatar : defaultAvatar;
-
-  return {
-    backgroundImage: `url(${avatarUser})`,
-  };
-};
+export const getAvatar = (avatar = defaultAvatar) => ({
+  backgroundImage: `url(${avatar})`
+});
