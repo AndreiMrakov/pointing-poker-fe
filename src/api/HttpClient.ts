bd968fd46f95f56fc64ac2d138b537000dc09d2e
Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
2021-09-07 15:18:46 +0300

    fix: error and add request interceptor

--- src/api/HttpClient.ts
+++ src/api/HttpClient.ts
@@ -1,30 +1,37 @@
-import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export class _HTTPClient {
 	static _instance: _HTTPClient;
 
-	private _axios!: AxiosInstance;
+	private _axios: AxiosInstance;
 
   constructor(public baseURL = ' ') {
     this._axios = axios.create({ baseURL: this.baseURL});
-    this._initializeResponseInterceptor();
+    this._initializeInterceptor();
 	}
 
-  static getClassInstance(){
+  static getInstance(){
     if (!this._instance) {
       this._instance = new _HTTPClient();    
     };
     return this._instance;
   };
 
-  private _initializeResponseInterceptor = () => {
+  private _initializeInterceptor = () => {
     this._axios.interceptors.response.use(
       this._handleResponse,
       this._handleError,
     );
+
+    this._axios.interceptors.request.use(
+      this._handleRequest,
+      this._handleError,
+    )
   };
 
   private _handleResponse = ({ data }: AxiosResponse) => data;
+
+  private _handleRequest = (config: AxiosRequestConfig) => config;
   
   private _handleError = (error: AxiosError) => {
     console.dir(error);
