4c3e907c0b4d55a62c2ce9ce716be90b03c6619d
MakFil <makfil1177@gmail.com>
2021-09-09 20:20:08 +0300

    feat: add Table component

---
+++ src/components/CommonArea/Table/Table.tsx
@@ -0,0 +1,46 @@
+import { PrimaryButton, SecondaryButton } from "@/components/Button";
+import React from "react";
+import styles from "./Table.module.scss";
+
+interface ITable {
+  isCardVisible: boolean;
+  selectedCardValue: string;
+  setIsCardIsVisible: (e: boolean) => void;
+  setSelectedCardValue: (e: string) => void;
+}
+
+export const Table: React.FC<ITable> = (
+  {
+    selectedCardValue,
+    isCardVisible, 
+    setIsCardIsVisible, 
+    setSelectedCardValue, 
+  }) => {
+
+  function showCards() {
+    setIsCardIsVisible(true);
+  }
+
+  function removeChosenCard() {
+    setSelectedCardValue('');
+    setIsCardIsVisible(false)
+  }
+
+  return (
+      <div className={styles.table}>
+        <p className={selectedCardValue || isCardVisible ? styles.none : ''}>
+        Pick your cards!
+        </p> 
+        <PrimaryButton 
+          className={selectedCardValue && !isCardVisible ? '' : styles.none} 
+          onClick={showCards}>
+          {'Show cards'}
+        </ PrimaryButton>
+        <SecondaryButton 
+          className={isCardVisible ? '' : styles.none} 
+          onClick={removeChosenCard}>
+          {'New voting'}
+        </SecondaryButton>
+      </div>
+  )
+}
