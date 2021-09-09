d70317735d65777a6032367304acacd73e388607
Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
2021-09-09 15:14:04 +0300

    feat: add class's export

--- src/NavMap/NavMap.ts
+++ src/NavMap/NavMap.ts
@@ -1,7 +1,9 @@
-export class NavMap {
+class NavMap {
   constructor (private baseUrl = '') {}
 
   home = () => `${this.baseUrl}/`;
   lobby = () => `${this.baseUrl}/lobby/:id`;
   game = () => `${this.baseUrl}/game/:id`;
 };
+
+export const navMap = new NavMap();
