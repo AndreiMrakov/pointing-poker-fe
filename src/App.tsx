fe8675696244a90e9e29c261be594801892e97b3
Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
2021-09-09 18:42:10 +0300

    feat: add routing into the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router,  Route,  Switch } from "react-router-dom";
+import { navMap } from './NavMap/NavMap';
 
 function App() {
-  return <></>;
+  return (
+  <>
+    <Router>
+      <Switch>
+        <Route path={navMap.home()} />
+        <Route path={navMap.lobby()} />
+        <Route path={navMap.game()} />
+      </Switch>
+    </Router>
+  </>
+  );
 }
 
 export default App;
