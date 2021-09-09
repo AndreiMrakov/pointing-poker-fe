1e660c85ebb7a4d4905b2314dd5cb5a190b0aa63
MakFil <makfil1177@gmail.com>
2021-09-09 20:16:25 +0300

    refactor: change game page content

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { PanelVoteCards } from "@/components/VoteCard";
 import { PanelUsers } from "@/components/User";
 import styles from './Game.module.scss';
 import { PrimaryButton, SecondaryButton } from "@/components/Button";
+import { CommonArea } from "@/components/CommonArea";
 
 const NAMES_BTN = {
   run: "run round",
@@ -12,6 +13,9 @@ const NAMES_BTN = {
 
 export const Game = () => {
   const title = 'Name room';
+
+  const [isCardVisible, setIsCardIsVisible] = useState(false);
+  const [selectedCardValue, setSelectedCardValue] = useState('');
 
   const handlerRun = () => {
     console.log("handlerRun");
@@ -27,27 +31,40 @@ export const Game = () => {
 
   return (
     <main className={styles.main}>
-      <div className={styles.wrapper}>
+      <article className={styles.wrapper}>
         <h1>{title}</h1>
-        <div className={styles.groupBtn}>
+        <section className={styles.groupBtn}>
           <PrimaryButton
+            className={styles.singleBtn}
             onClick={handlerRun}
           >
             {NAMES_BTN.run}
           </PrimaryButton>
           <PrimaryButton
+            className={styles.singleBtn}
             onClick={handlerRestart}
           >
             {NAMES_BTN.restart}
           </PrimaryButton>
           <SecondaryButton
+            className={styles.singleBtn}
             onClick={handlerStop}
           >
             {NAMES_BTN.stop}
           </SecondaryButton>
-        </div>
-        <PanelVoteCards />
-      </div>
+        </section>
+          <CommonArea 
+            isCardVisible={isCardVisible}
+            selectedCardValue={selectedCardValue}
+            setIsCardIsVisible={setIsCardIsVisible}
+            setSelectedCardValue={setSelectedCardValue}
+          />
+        <PanelVoteCards 
+          setSelectedCardValue={setSelectedCardValue}
+          selectedCardValue={selectedCardValue}
+          isCardVisible={isCardVisible}
+        />
+      </article>
       <PanelUsers />
     </main>
   );
