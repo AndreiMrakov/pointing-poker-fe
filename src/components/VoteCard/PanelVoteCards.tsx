f344023f19614c826b0ce0ea8a1bcd67bfefa313
MakFil <makfil1177@gmail.com>
2021-09-09 20:18:12 +0300

    refactor: PanelVoteCards and VoteCard

--- src/components/VoteCard/PanelVoteCards.tsx
+++ src/components/VoteCard/PanelVoteCards.tsx
@@ -3,10 +3,38 @@ import { VoteCard } from "./VoteCard";
 import styles from "./VoteCard.module.scss";
 import cards from "@/mocks/cards.json";
 
-export const PanelVoteCards = () => {
+interface IPanelVoteCards {
+  selectedCardValue: string;
+  setSelectedCardValue: (e: string) => void;
+  isCardVisible: boolean
+}
+
+export const PanelVoteCards: React.FC<IPanelVoteCards> = ({
+  selectedCardValue, 
+  setSelectedCardValue,
+  isCardVisible
+}) => {
+
+  function onCardClick(event: React.MouseEvent): void {
+    const currentElem = event.target as HTMLElement;
+    setSelectedCardValue(currentElem.textContent!)
+  }
+
+
   return (
     <ul className={styles.listCards}>
-      {cards.map(card => <VoteCard card={card} key={card.id} />)}
+      {cards.map(card => 
+        <VoteCard 
+          card={card} 
+          key={card.id} 
+          styleName=
+          {
+            `${card.score === +selectedCardValue ? styles.active : ''} 
+             ${isCardVisible ? styles.unactive : ''}
+          `} 
+          onCardClick={onCardClick}
+        />)
+      }
     </ul>
   );
 };
--- src/components/VoteCard/VoteCard.tsx
+++ src/components/VoteCard/VoteCard.tsx
@@ -4,14 +4,21 @@ import styles from './VoteCard.module.scss';
 
 type Props = {
   card: CardType;
+  onCardClick: (e:React.MouseEvent) => void;
+  styleName: any
 };
 
-export const VoteCard: FC<Props> = ({ card }) => {
+export const VoteCard: FC<Props> = (
+  { 
+    card, 
+    onCardClick, 
+    styleName 
+  }) => {
+
   return (
-    <div className={styles.card}>
-      <div className={styles.nameTop}>{card.name}</div>
-      <h3 className={styles.score}>{card.score}</h3>
-      <div className={styles.nameBottom}>{card.name}</div>
-    </div>
+    <li className={`${styles.card} ${styleName}`} 
+        onClick={onCardClick}>
+        {card.score}
+    </li>
   );
 };
