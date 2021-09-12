5827d910761beb371a75931780e8c4ca691b8e2d
Andrei Kaptsiuh <kaptsiuhandrei@gmail.com>
2021-09-12 10:05:30 +0300

    feat: add eslint for typescript, react and jest

--- src/test/HttpClient.test.ts
+++ src/test/HttpClient.test.ts
@@ -1,11 +1,10 @@
-import { httpClient } from '../api/HttpClient';
-import { HTTPClient } from '../api/HttpClient';
+import { httpClient, HTTPClient } from '../api/HttpClient';
 
 describe('Create class', () => {
   test('Should be created', () => {
     expect(httpClient).not.toBe(undefined);
   });
-  
+
   test('Should be one class', () => {
     const httpClientDouble = HTTPClient.getInstance();
     expect(httpClient === httpClientDouble).toBe(true);
@@ -16,7 +15,7 @@ describe('Base url', () => {
   test('Should return url', () => {
     expect(httpClient.http.defaults.baseURL === '').toBe(true);
     httpClient.url = 'url';
-    
+
     expect(httpClient.http.defaults.baseURL === 'url').toBe(true);
   });
 });
