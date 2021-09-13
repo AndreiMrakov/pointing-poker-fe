10457e7bdef44e7a553ba00be6fb7ea221da5097
Yury Bialou <Pradd.by@gmail.com>
2021-09-13 13:26:08 +0300

    fix: fix path in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Game } from './pages/Game';
 import styles from './App.module.scss';
 import { BrowserRouter as Router,  Redirect,  Route,  Switch } from "react-router-dom";
-import { navMap } from './NavMap';
+import { navMap } from './untils/NavMap';
 
 export function App() {
   return (
   <div className={styles.container}>
     <Router>
       <Switch>
-        <Route path={navMap.home()}></Route>
-        <Route path={navMap.lobby()}></Route>
+        <Route exact path={navMap.home()}></Route>
+        <Route exact path={navMap.lobby()}></Route>
         <Route path={navMap.game()}>
           <Game />
         </Route>
