26ed9d8d739d9dd0b2077a809ef2529d108f7681
Yury Bialou <Pradd.by@gmail.com>
2021-09-14 14:05:43 +0300

    fix: fix rules eslint for User

--- src/components/User/PanelUsers.tsx
+++ src/components/User/PanelUsers.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { User } from "./User";
-import styles from "./User.module.scss";
-import users from "@/mocks/users.json";
+import React from 'react';
+import { User } from './User';
+import styles from './User.module.scss';
+import users from '@/mocks/users.json';
 
-export const PanelUsers = () => {
-  return (
-    <ul className={styles.listUsers}>
-      {users.map(user => <User user={user} key={user.id} />)}
-    </ul>
-  );
-};
+export const PanelUsers = () => (
+  <ul className={styles.listUsers}>
+    {users.map((user) => <User user={user} key={user.id} />)}
+  </ul>
+);
