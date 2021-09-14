1f76594f8de72c7927bb656c796479e2291281ce
Yury Bialou <Pradd.by@gmail.com>
2021-09-14 14:14:34 +0300

    fix: fix rules eslint for VoteCard

--- src/components/VoteCard/PanelVoteCards.tsx
+++ src/components/VoteCard/PanelVoteCards.tsx
@@ -1,31 +1,28 @@
-import React from "react";
-import { VoteCard } from "./VoteCard";
-import styles from "./VoteCard.module.scss";
-import { cards } from "@/mocks/cards";
+import React, { Dispatch, SetStateAction } from 'react';
+import { VoteCard } from './VoteCard';
+import styles from './VoteCard.module.scss';
+import { cards } from '@/mocks/cards';
 
 interface IPanelVoteCardsProps {
   selectedCardValue: string;
-  setSelectedCardValue: (e: string) => void;
+  setSelectedCardValue: Dispatch<SetStateAction<string>>;
   isCardOpened: boolean
 }
 
 export const PanelVoteCards: React.FC<IPanelVoteCardsProps> = ({
-  selectedCardValue, 
+  selectedCardValue,
   setSelectedCardValue,
-  isCardOpened
-}) => {
-
-  return (
-    <ul className={styles.listCards}>
-      {cards.map(card => 
-        <VoteCard 
-          score={card} 
-          key={card} 
-          setSelectedCardValue={setSelectedCardValue}
-          isCardOpened={isCardOpened}
-          selectedCardValue={selectedCardValue}
-        />)
-      }
-    </ul>
-  );
-};
+  isCardOpened,
+}) => (
+  <ul className={styles.listCards}>
+    {cards.map((card) => (
+      <VoteCard
+        score={card}
+        key={card}
+        setSelectedCardValue={setSelectedCardValue}
+        isCardOpened={isCardOpened}
+        selectedCardValue={selectedCardValue}
+      />
+    ))}
+  </ul>
+);
