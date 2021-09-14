4b95c099a245d0630e5c17d6a6d06de2cfce5284
Yury Bialou <Pradd.by@gmail.com>
2021-09-14 14:19:12 +0300

    fix: fix rules eslint for App and other

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import { Game } from './pages/Game';
 import styles from './App.module.scss';
-import { BrowserRouter as Router,  Redirect,  Route,  Switch } from "react-router-dom";
 import { navMap } from './untils/NavMap';
 
 export function App() {
   return (
-  <div className={styles.container}>
-    <Router>
-      <Switch>
-        <Route exact path={navMap.home()}></Route>
-        <Route path={navMap.lobby()}></Route>
-        <Route path={navMap.game()}>
-          <Game />
-        </Route>
-        <Redirect to={navMap.home()} />
-      </Switch>
-    </Router>
-  </div>
+    <div className={styles.container}>
+      <Router>
+        <Switch>
+          <Route exact path={navMap.home()} />
+          <Route path={navMap.lobby()} />
+          <Route path={navMap.game()}>
+            <Game />
+          </Route>
+          <Redirect to={navMap.home()} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
