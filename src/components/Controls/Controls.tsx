9e6b9272b79b123f885ab1f1710d58d9add9fe00
Yury Bialou <Pradd.by@gmail.com>
2021-09-14 14:03:40 +0300

    fix: fix rules eslint for Controls

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,28 +1,27 @@
-import React  from "react";
-import { PrimaryButton, SecondaryButton } from "@/components/Button";
+import React from 'react';
+import { PrimaryButton, SecondaryButton } from '@/components/Button';
 import styles from './Controls.module.scss';
 
 const NAMES_BTN = {
-  run: "run round",
-  restart: "restart round",
-  stop: "stop game"
+  run: 'run round',
+  restart: 'restart round',
+  stop: 'stop game',
 };
-
 
 export const Controls = () => {
   const handlerRun = () => {
-    console.log("handlerRun");
-  };
-  
-  const handlerRestart = () => {
-    console.log("handlerRestart");
-  };
-  
-  const handlerStop = () => {
-    console.log("handlerStop");
+    console.log('handlerRun');
   };
 
-  return(
+  const handlerRestart = () => {
+    console.log('handlerRestart');
+  };
+
+  const handlerStop = () => {
+    console.log('handlerStop');
+  };
+
+  return (
     <section className={styles.groupBtn}>
       <PrimaryButton
         className={styles.singleBtn}
@@ -43,5 +42,5 @@ export const Controls = () => {
         {NAMES_BTN.stop}
       </SecondaryButton>
     </section>
-  )
-}
+  );
+};
