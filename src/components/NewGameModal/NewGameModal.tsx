39e96542bb99aec4f023ab1d5a8feaf0b9260830
Andrei <kaptsiuhandrei@gmail.com>
2021-09-15 14:23:20 +0300

    feat: add close modal and add start new game button

--- src/components/NewGameModal/NewGameModal.tsx
+++ src/components/NewGameModal/NewGameModal.tsx
@@ -4,7 +4,12 @@ import { Input } from '../Input';
 import { Select } from '../Select';
 import styles from './NewGameModal.module.scss';
 
-export const NewGameModal: React.FC = () => {
+interface INewGameModalProps {
+  show: boolean;
+  onClick: () => void;
+};
+
+export const NewGameModal: React.FC<INewGameModalProps> = ({ show, onClick }) => {
   const [form, setForm] = useState({
     gameName: '',
     voteSystem: voteOptions[0],
@@ -22,41 +27,49 @@ export const NewGameModal: React.FC = () => {
     event.preventDefault();
     console.log('submit', form);
   };
+
+  const closeModal = (elem: HTMLElement) => {
+    if (elem.classList.contains(styles.modal)) onClick();
+  };
   
   return (
-    <section className={styles.modal}>
-      <h3 className={styles.title}>
-        Choose a name and a voting system for your game.
-      </h3>
-
-      <form className={styles.form} onSubmit={submitHandler}>
-        <Input 
-          type="text" 
-          label="Game's name" 
-          name="gameName"
-          onChange={formHandler} 
-        />
-        <Select 
-          name="voteSystem"
-          label="Voting system"
-          option={voteOptions} 
-          value={form.voteSystem}
-          onChange={formHandler}
-        />
-        <Select 
-          name="dealer"
-          label="Who can show cards?"
-          option={dealerOptions} 
-          value={form.dealer}
-          onChange={formHandler}
-        />
-        <Input 
-          type="submit" 
-          name="Create game" 
-          value="Create game"
-          className={styles.form_submit}
-        />
-      </form>
-    </section>
+    <div 
+      className={show ? styles.modal : styles.modal_close}
+      onClick={(e) => closeModal(e.target as HTMLElement)}
+    >
+      <section className={styles.modal_content}>
+        <h3 className={styles.title}>
+          Choose a name and a voting system for your game.
+        </h3>
+        <form className={styles.form} onSubmit={submitHandler}>
+          <Input 
+            type="text" 
+            label="Game's name" 
+            name="gameName"
+            onChange={formHandler} 
+          />
+          <Select 
+            name="voteSystem"
+            label="Voting system"
+            option={voteOptions} 
+            value={form.voteSystem}
+            onChange={formHandler}
+          />
+          <Select 
+            name="dealer"
+            label="Who can show cards?"
+            option={dealerOptions} 
+            value={form.dealer}
+            onChange={formHandler}
+          />
+          <Input 
+            type="submit" 
+            name="Create game" 
+            value="Create game"
+            className={styles.form_submit}
+          />
+        </form>
+      </section>
+    </div>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,23 @@
+import { PrimaryButton } from "@/components/Button";
 import { NewGameModal } from "@/components/NewGameModal";
-import React from "react";
+import React, { useState } from "react";
 import styles from './Home.module.scss';
 
 export const Home: React.FC = () => {
+  const [modal, setModal] = useState(false);
 
+  const showModal = () => {
+    setModal(!modal);
+  };
 
   return (
     <main className={styles.home}>
       <article className={styles.wrapper}>
-        <NewGameModal />
+        <h2 className={styles.title}>Pointing Poker</h2>
+        <PrimaryButton onClick={showModal}>
+          Create new game
+        </PrimaryButton>
+        <NewGameModal show={modal} onClick={showModal} />
       </article>
     </main>
   );
