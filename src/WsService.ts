df9194820f7f782947ecc48874102b82c1187dcb
MakFil <makfil1177@gmail.com>
2021-09-15 20:02:03 +0300

    feat: add connection with BE socketService

--- src/WsService.ts
+++ src/WsService.ts
@@ -3,3 +3,6 @@ import io from 'socket.io-client';
 const URL = process.env.REACT_APP_SOCKET_URL as string;
 
 export const socketService = io(URL);
+
+export const JOIN_USER = 'JOIN_USER';
+export const LEAVE_USER = 'LEAVE_USER';
--- src/components/User/PanelUsers.tsx
+++ src/components/User/PanelUsers.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import { User } from './User';
+import React, { useEffect, useState } from 'react';
+import { IUser } from '@/interfaces';
+import { SingleUser } from '@/helpers/getSingleUser';
 import styles from './User.module.scss';
-import users from '@/mocks/users.json';
+import { JOIN_USER, LEAVE_USER, socketService } from '@/WsService';
+import { User } from './User';
 
-export const PanelUsers = () => (
-  <ul className={styles.listUsers}>
-    {users.map((user) => <User user={user} key={user.id} />)}
-  </ul>
-);
+export const PanelUsers = () => {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    socketService.on(JOIN_USER, (msg) => {
+      const newUser = new SingleUser(msg);
+      setUsers((usrs) => [...usrs, newUser]);
+    });
+
+    socketService.on(LEAVE_USER, (id) => {
+      setUsers((usrs) => usrs.filter((usr) => usr.id !== id));
+    });
+  }, []);
+
+  return (
+    <ul className={styles.listUsers}>
+      {users.map((user) => <User user={user} key={user.id} />)}
+    </ul>
+  );
+};
