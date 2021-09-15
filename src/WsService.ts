8df118f3c360a0522f52713e2914aa75df5ce28f
MakFil <makfil1177@gmail.com>
2021-09-15 23:11:31 +0300

    refactor: eslinrc according enum rules

--- src/WsService.ts
+++ src/WsService.ts
@@ -4,9 +4,7 @@ const URL = process.env.REACT_APP_SOCKET_URL as string;
 
 export const socketService = io(URL);
 
-/* eslint-disable */
 export enum SocketEvents {
   joinUser = 'joinUser',
   leaveUser = 'leaveUser'
 }
-/* eslint-disable */
