ba8b2f4ae1a9c0169c3a8ee21519cdfd01cd6b30
MakFil <makfil1177@gmail.com>
2021-09-16 12:24:32 +0300

    refactor: rename socketService.ts and place it to services

--- src/services/socketService/socketService.ts
+++ src/services/socketService/socketService.ts
@@ -3,8 +3,3 @@ import io from 'socket.io-client';
 const URL = process.env.REACT_APP_SOCKET_URL as string;
 
 export const socketService = io(URL);
-
-export enum SocketEvents {
-  joinUser = 'joinUser',
-  leaveUser = 'leaveUser'
-}
