5063596ece6b5f7dd3931cd3c5cabbb8146b172d
MakFil <makfil1177@gmail.com>
2021-09-19 21:22:15 +0300

    refactor: combine user selectors as userSelectors object

--- src/store/selectors/userSelectors.ts
+++ src/store/selectors/userSelectors.ts
@@ -2,9 +2,11 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../store';
 
 const user = (state: RootState) => state.userReducer.user;
-const settings = (state: RootState) => state.userReducer.gameSettings;
+const settings = (state: RootState) => state.gameReducer.gameSettings;
 
-export const userId = createSelector(user, (state) => state.id);
-export const userName = createSelector(user, (state) => state.name);
-export const userRoom = createSelector(user, (state) => state.room);
-export const gameSettings = createSelector(settings, (state) => state);
+export const userSelectors = {
+  userId: createSelector(user, (state) => state.id),
+  userName: createSelector(user, (state) => state.name),
+  userRoom: createSelector(user, (state) => state.room),
+  gameSettings: createSelector(settings, (state) => state),
+};
