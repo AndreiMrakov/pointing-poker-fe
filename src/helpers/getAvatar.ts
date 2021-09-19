f1ae9515ba4be194301bf70ba1ce56384540d47c
Andrei <kaptsiuhandrei@gmail.com>
2021-09-19 19:48:09 +0300

    fix: return type

--- src/helpers/getAvatar.ts
+++ src/helpers/getAvatar.ts
@@ -1,5 +1,6 @@
+import { CSSProperties } from 'react';
 import defaultAvatar from '@/assets/images/default-avatar.svg';
 
-export const getAvatar = (avatar: string): {} => ({
+export const getAvatar = (avatar: string): CSSProperties => ({
   backgroundImage: `url(${avatar || defaultAvatar})`,
 });
