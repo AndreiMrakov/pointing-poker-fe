ff362fc9e905f1f8c2c184cc25af4e059de0a8b8
MakFil <makfil1177@gmail.com>
2021-09-26 12:48:04 +0300

    feat: add UseAppDispatch

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+import { messages } from './reducers/messagesReducer';
 import { getEnvValue } from '@/helpers/getEnvValue';
 import { user } from '@/store/reducers/userReducer';
 import { gameSettings } from './reducers/gameSettingsReducer';
 
 export const store = configureStore({
-  reducer: { user, gameSettings },
+  reducer: { user, gameSettings, messages },
   middleware: getDefaultMiddleware({
     immutableCheck: true,
     serializableCheck: false,
@@ -15,3 +17,5 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = (): ReturnType<typeof useDispatch> => useDispatch<AppDispatch>();
