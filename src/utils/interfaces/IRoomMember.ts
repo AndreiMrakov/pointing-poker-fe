f49830243b4741d51e4580d83d5bc03a1ebe12e1
MakFil <makfil1177@gmail.com>
2021-09-27 13:04:58 +0300

    feat: add IRoomMember

---
+++ src/utils/interfaces/IRoomMember.ts
@@ -0,0 +1,6 @@
+export interface IRoomMember {
+  name: string;
+  roomUserId: string;
+  role?: string;
+  score?: string;
+}
--- src/utils/interfaces/index.ts
+++ src/utils/interfaces/index.ts
@@ -2,3 +2,4 @@ export type { IUser } from './IUser';
 export type { IMessage } from './IMessage';
 export type { ISendMessage } from './ISendMessage';
 export type { IMessageFromBE } from './IMessageFromBE';
+export type { IRoomMember } from './IRoomMember';
