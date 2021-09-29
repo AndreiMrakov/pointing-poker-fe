e9653f98d6e3b430dac5a854a8a738e3d425283b
Andrei <kaptsiuhandrei@gmail.com>
2021-09-29 12:53:40 +0300

    fix: initial state for tasks

--- src/store/reducers/tasksReducer.ts
+++ src/store/reducers/tasksReducer.ts
@@ -1,6 +1,11 @@
 import { createReducer } from '@reduxjs/toolkit';
 
-const initialStateTasks = {};
+const initialStateTasks = {
+  id: 0,
+  title: '',
+  score: 0,
+  isActive: false,
+};
 
 export const tasks = createReducer(initialStateTasks, (builder) => {
   builder
