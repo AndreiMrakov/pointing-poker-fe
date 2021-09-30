2fe201008391fc1de334e0cf9afae579d7270fea
MakFil <makfil1177@gmail.com>
2021-09-30 17:55:17 +0300

    refactor: Modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,23 +1,31 @@
 import React, { MouseEventHandler, ReactNode } from 'react';
+import classNames from 'classnames';
 import styles from './Modal.module.scss';
 
 interface IModalProps {
   children: ReactNode;
-  show: boolean;
-  onClick: () => void;
+  show?: boolean;
+  onClick?: () => void;
+  styleName?: string;
 }
 
-export const Modal: React.FC<IModalProps> = ({ children, show, onClick }) => {
+export const Modal: React.FC<IModalProps> = ({
+  children, show, onClick, styleName,
+}) => {
   const closeModal: MouseEventHandler = (event) => {
     const elem = event.target as HTMLElement;
-    if (elem.classList.contains(styles.modal)) {
+    if (elem.classList.contains(styles.modal) && onClick) {
       onClick();
     }
   };
 
   return (
     <div
-      className={show ? styles.modal : styles.modal_close}
+      className={classNames(styleName,
+        {
+          [styles.modal]: show,
+          [styles.modal_close]: !show,
+        })}
       onClick={closeModal}
       role="button"
       aria-hidden="true"
