3354011478364fb38866cd71018bbe51f8922c0a
Andrei <kaptsiuhandrei@gmail.com>
2021-09-30 20:38:21 +0300

    feat: add handlers to catch all socket events related to tasks

--- src/components/Issues/Issues.tsx
+++ src/components/Issues/Issues.tsx
@@ -4,15 +4,35 @@ import { roomStateSelectors, tasksSelectors } from '@/store/selectors';
 import styles from './Issues.module.scss';
 import { useAppDispatch } from '@/store';
 import { taskActions } from '@/store/actions/taskActions';
+import { socketService } from '@/services';
+import { SocketEvent } from '@/utils/enums';
 
 export const Issues: React.FC = () => {
   const tasks = useSelector(tasksSelectors.tasks);
   const roomId = useSelector(roomStateSelectors.roomId);
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
+  const updateTasks = () => {
     dispatch(taskActions.getTasks(roomId));
+  };
+
+  useEffect(() => {
+    updateTasks();
   }, [roomId]);
+
+  useEffect(() => {
+    socketService.on(SocketEvent.TaskCreate, updateTasks);
+    socketService.on(SocketEvent.TaskDelete, updateTasks);
+    socketService.on(SocketEvent.TaskUpdateScore, updateTasks);
+    socketService.on(SocketEvent.TaskUpdateActive, updateTasks);
+
+    return () => {
+      socketService.off(SocketEvent.TaskCreate, updateTasks);
+      socketService.off(SocketEvent.TaskDelete, updateTasks);
+      socketService.off(SocketEvent.TaskUpdateScore, updateTasks);
+      socketService.off(SocketEvent.TaskUpdateActive, updateTasks);
+    };
+  }, []);
 
   return (
     <section className={styles.issues}>
--- src/utils/enums/SocketEvent.ts
+++ src/utils/enums/SocketEvent.ts
@@ -6,5 +6,9 @@ export enum SocketEvent {
   FinishGame = 'finishGame',
   UserVote = 'userVote',
   CreateGame = 'createGame',
-  MessageCreate = 'message:create'
+  MessageCreate = 'message:create',
+  TaskCreate = 'task_create',
+  TaskDelete = 'task_delete',
+  TaskUpdateScore = 'task_set_score',
+  TaskUpdateActive = 'task_set_active'
 }
