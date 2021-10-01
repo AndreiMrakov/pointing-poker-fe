fe7b3c965c02b1fe73c48bb2a2b5cd11393c5a18
Andrei <kaptsiuhandrei@gmail.com>
2021-10-02 01:25:52 +0300

    fix: get tasks action

--- src/store/actions/taskActions.ts
+++ src/store/actions/taskActions.ts
@@ -2,12 +2,14 @@ import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 import { httpClient } from '@/api/HttpClient';
 import { ITask, ITaskFromBE } from '@/utils/interfaces';
 import { TaskModel } from '@/models';
+import { roomStateSelectors } from '../selectors';
+import { RootState } from '..';
 
 export const taskActions = {
   getTasks: createAsyncThunk('[TASKS]:getTasks',
     async (_, { rejectWithValue, getState }) => {
       try {
-        const roomId = getState();
+        const roomId = roomStateSelectors.roomId(getState() as RootState);
         const tasksFromBE: ITaskFromBE[] = await httpClient.http.get(`/api/task?roomId=${roomId}`);
         const tasks = tasksFromBE.map((task) => new TaskModel(task));
         return tasks;
