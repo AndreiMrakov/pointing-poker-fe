d8472498d61cd7a0059cff401ac596253dca3865
MakFil <makfil1177@gmail.com>
2021-10-04 21:00:50 +0300

    fix: task field from BE

--- src/utils/interfaces/ITaskFromBE.ts
+++ src/utils/interfaces/ITaskFromBE.ts
@@ -3,7 +3,8 @@ export interface ITaskFromBE {
   title: string;
   roomId: string;
   description?: string;
-  isActive: boolean;
+  // eslint-disable-next-line camelcase
+  is_active: boolean;
   score?: string;
   avgScore?: number;
   createdAt: Date;
